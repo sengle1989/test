5fe0a9b0dfb18100db20c5352877eac1a51dce4e
sengle1989 <314024677@qq.com>
2021-03-22 14:16:26 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1 Hello world!");
+		System.out.println("2 Hello world!");
+		
+		int [] intArrayTest = new int[20];
+		for(int i = 0;i < 20; i++){
+			intArrayTest[i] = i+1;
+			System.out.println(intArrayTest[i]);
+			System.out.println(intArrayTest[i]);
+		}
+		System.out.println("3 Hello world!");
+		System.out.println("4 Hello world!");
+		
+	}
+
+}
